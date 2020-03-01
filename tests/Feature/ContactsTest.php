5ca8632b65bae9659cad91a463dbae6078343acc
NerminKarapandzic <nermin_biz@hotmail.com>
2020-03-01 17:12:54 +0100

    Email & date fields format testing

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Contact;
+use Carbon\Carbon;
 
 class ContactsTest extends TestCase
 {
@@ -20,7 +21,6 @@ class ContactsTest extends TestCase
 
         $this->assertEquals('Test name', $contact->name);
         $this->assertEquals('test@email.com',$contact->email);
-        $this->assertEquals('01/05/1996',$contact->birthday);
         $this->assertEquals('ABC string', $contact->company);
 
     }
@@ -37,12 +37,32 @@ class ContactsTest extends TestCase
         });
     }
 
+    /** @test */
+    public function must_be_email()
+    {
+        $response = $this->post('/api/contacts',array_merge($this->data(), ['email' => 'NOT AN EMAIL']));
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0,Contact::all());
+    }
+
+    /** @test */
+    public function date_is_date()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('/api/contacts',$this->data());
+
+        $this->assertCount(1,Contact::all());
+        $this->assertInstanceOf(Carbon::class,Contact::first()->birthday);
+        $this->assertEquals('01-05-1996',Contact::first()->birthday->format('d-m-Y'));
+    }
+
     private function data()
     {
         return array(
             'name' => 'Test name',
             'email' => 'test@email.com',
-            'birthday' => '01/05/1996',
+            'birthday' => '05/01/1996',
             'company' => 'ABC string'
         );
     }
